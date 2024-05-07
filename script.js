dc82cd414a54f05fbdb14b311e85b3e9e6911b56
Gabriel de Melo Braz <153194983+BielMeloDev@users.noreply.github.com>
2024-05-07 09:01:15 -0300

    script da calculadora

---
+++ script.js
@@ -0,0 +1,17 @@
+var v1=document.getElementById("v1")
+var v2=document.getElementById("v2")
+var result=document.getElementById("result")
+
+function somar() {
+    result.value=parseFloat(v1.value) + result.value==parseFloat(v2.value) 
+}
+
+function subtrair() {
+    result.value=parseFloat(v1.value) - result.value==parseFloat(v2.value) 
+}
+function dividir() {
+    result.value=parseFloat(v1.value) - result.value==parseFloat(v2.value) 
+}
+function multiplicar() {
+    result.value=parseFloat(v1.value) - result.value==parseFloat(v2.value) 
+}
